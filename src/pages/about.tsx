b8481c661ddc1053ecf3f396b717a5a0292f226f
Samyak Bhuta <samyak.bhuta@gmail.com>
2020-07-26 20:41:17 +0530

    Changes in content
    * Added new section with photgraphs
    * Removed default section
    * Change in about page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,10 +15,23 @@ const About = () => {
     <Layout>
       <SEO title="About | Jodie" desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects." />
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
-        <h1>Hi. I'm LekoArts!</h1>
-        <p>You can visit my website or my other Gatsby projects:</p>
+        <h1>Hi. I'm Loki Verma!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>
-          <a href="https://www.lekoarts.de">Website</a> - <a href="https://themes.lekoarts.de">More projects</a>
+          <ul>
+            <li>
+              Visit me at <a href="https://lightingpicture.com/">Website</a>
+            </li>
+            <li>
+              Call me on <a href="tel:+918460404349">+91 8460 40 43 49</a>
+            </li>
+            <li>
+              Send me a mail at <a href="mailto:lightingpictures1@gmail.com">lightingpictures1@gmail.com</a>
+            </li>
+            <li>
+              Send a message on my <a href="https://api.whatsapp.com/send?phone=918460404349">WhatsApp</a>
+            </li>
+          </ul>
         </p>
       </AnimatedBox>
     </Layout>
